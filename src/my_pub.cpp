a43d727191782e3c8815db1722f4e8581e8a6c16
eashangallage <eashanpol@gmail.com>
2024-02-23 23:53:25 -0500

    Add new directory

--- src/my_pub.cpp
+++ src/my_pub.cpp
@@ -15,6 +15,7 @@ This code publishes a hardcoded value to the to cmd_vel. It is a good way to che
 // #include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp> // message type diffbot uses
 #include <sensor_msgs/msg/joy.hpp> // message type used by the joy_node
+#include <ros_phoenix/msg/motor_control.hpp>             // message type used by the joy_node
 
 using namespace std::chrono_literals;
 
@@ -27,7 +28,7 @@ class CmdPublisher : public rclcpp::Node // creates the node class CmdPublisher 
 public: // public constructor
     CmdPublisher() : Node("cmd_publisher"), count_(0) // renames the node to cmd_publisher and initializes count_ to 0.
     {
-        publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/diffbot_base_controller/cmd_vel", 10);
+        publisher_ = this->create_publisher<ros_phoenix::msg::MotorControl>("/talon_right/set", 10);
         timer_ = this->create_wall_timer(
             50ms, std::bind(&CmdPublisher::timer_callback, this)); // Next, timer_ is initialized, causes the timer_callback function to execute every 50ms.
     }
@@ -36,30 +37,29 @@ private:
     void timer_callback() // timer_callback function: the message data is set and the messages are actually published
     {
         // auto message = std_msgs::msg::String();
-        auto message = geometry_msgs::msg::TwistStamped();
+        auto message = ros_phoenix::msg::MotorControl();
         // message.data = "Hello, world! " + std::to_string(count_++); // Creates the message
-        message.header.stamp.sec = 0;
-        message.header.stamp.nanosec = 0;
-        // message.header.frame_id = ''; // for twiststamped messages the headers can be predefined
-        message.twist.linear.x = 1;
-        message.twist.linear.y = 0;
-        message.twist.linear.z = 0;
-        message.twist.angular.x = 0.0;
-        message.twist.angular.y = 0.0;
-        message.twist.angular.z = 1.8;
-        auto pub_str = "linear: [" + std::to_string(message.twist.linear.x) + ", " +
-                   std::to_string(message.twist.linear.y) + ", " +
-                   std::to_string(message.twist.linear.z) + "], angular: [" +
-                   std::to_string(message.twist.angular.x) + ", " +
-                   std::to_string(message.twist.angular.y) + ", " +
-                   std::to_string(message.twist.angular.z) + "]";
+        message.mode = 0;
+        message.value = 1.0;
+        auto pub_str = "{ mode: " + std::to_string(message.mode) + ", value: " + std::to_string(message.value) + "}";
+        // // message.header.frame_id = ''; // for twiststamped messages the headers can be predefined
+        // message.twist.linear.z = 0;
+        // message.twist.angular.x = 0.0;
+        // message.twist.angular.y = 0.0;
+        // message.twist.angular.z = 1.8;
+        // auto pub_str = "linear: [" + std::to_string(message.twist.linear.x) + ", " +
+        //            std::to_string(message.twist.linear.y) + ", " +
+        //            std::to_string(message.twist.linear.z) + "], angular: [" +
+        //            std::to_string(message.twist.angular.x) + ", " +
+        //            std::to_string(message.twist.angular.y) + ", " +
+        //            std::to_string(message.twist.angular.z) + "]";
         RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", pub_str.c_str()); // RCLCPP_INFO macro ensures messages are published and printed to the console
         publisher_->publish(message);                                              // publishing the message
     }
     // declaration of the timer, publisher, and counter fields.
     rclcpp::TimerBase::SharedPtr timer_;
     //   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr publisher_;
+    rclcpp::Publisher<ros_phoenix::msg::MotorControl>::SharedPtr publisher_;
     size_t count_;
 };
 
--- src/pubsub_node.cpp
+++ src/pubsub_node.cpp
@@ -7,18 +7,14 @@ This program maps the input of the joy_node to to run the ros2_controls_demos/ex
 #include <memory>
 #include <string>
 
-#include "rclcpp/rclcpp.hpp"
 // following headers can be used depending on the input and output messag type
-// #include "std_msgs/msg/string.hpp"
-// #include "geometry_msgs/msg/twist.hpp"
-// #include "std_msgs/msg/string.hpp"             //built-in message type you will use to publish data
-// #include <geometry_msgs/msg/twist.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp> // message type diffbot uses
-#include <sensor_msgs/msg/joy.hpp>             // message type used by the joy_node
+#include "rclcpp/rclcpp.hpp"
+#include <sensor_msgs/msg/joy.hpp>              // message type used by the joy_node
+#include <ros_phoenix/msg/motor_control.hpp>    // message type used by the joy_node
 
 using std::placeholders::_1;
 
-// axes and buttons of 8BitDo Pro 2
+// axes and buttons of 8PowerA Nintendo Switch gamepad
 
 enum Axis
 {
@@ -26,8 +22,9 @@ enum Axis
   LEFT_STICK_Y = 1,
   LEFT_TRIGGER = 2,
   RIGHT_STICK_X = 3,
-  D_PAD_X = 4,
-  D_PAD_Y = 5
+  RIGHT_STICK_Y = 4,
+  D_PAD_Y = 5,
+  D_PAD_X = 6
 };
 enum Button
 {
@@ -59,13 +56,14 @@ public:
   {
     // Create the subscription.
     // The topic_callback function executes whenever data is published
-    // to the 'addison' topic.
+    // to the 'joy' topic.
     subscription_ = this->create_subscription<sensor_msgs::msg::Joy>(
         "joy", 10, std::bind(&Joy2Cmd::topic_callback, this, _1));
 
-    // Publisher publishes String messages to a topic named "addison2".
-    // The size of the queue is 10 messages.
-    publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/diffbot_base_controller/cmd_vel", 10);
+    // The size of the queue is 10 messages. 10 commands per second
+    publisher_talon_right = this->create_publisher<ros_phoenix::msg::MotorControl>("/talon_right/set", 10);
+
+    publisher_talon_left = this->create_publisher<ros_phoenix::msg::MotorControl>("/talon_left/set", 10);
   }
 
 private:
@@ -73,21 +71,36 @@ private:
   void topic_callback(const sensor_msgs::msg::Joy &msg) const
   {
     // Declare message in the publishing message type
-    auto message = geometry_msgs::msg::TwistStamped();
+    auto message_talon_right = ros_phoenix::msg::MotorControl();
+    auto message_talon_left = ros_phoenix::msg::MotorControl();
 
-    // setting the required to inputs to the output
-    message.twist.linear.x = msg.axes[LEFT_STICK_Y];
-    message.twist.angular.z = msg.axes[LEFT_STICK_X];
+    // setting the required to parameters for the message interface
+    message_talon_right.mode = 0; // 0: percentage output, 1: velocity
+    message_talon_left.mode = 0;
 
-    // Publish the message
-    publisher_->publish(message);
+    // inputs are scales are [-1,1], output scale [-0.2,0.2]. Hence, input is divided by 5
+    double fwd = msg.axes[LEFT_STICK_Y]/5; // feedback from the gamepad
+    double turn = msg.axes[LEFT_STICK_X]/5;
+
+    // setting the proper velocity
+    message_talon_right.value = fwd + turn; 
+    message_talon_left.value = fwd - turn; 
+
+    // Publish the message to diffbot
+    publisher_talon_right->publish(message_talon_right);
+
+    // Publish the message to rrbot
+    publisher_talon_left->publish(message_talon_left);
   }
 
   // Declare the subscription attribute
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
 
-  // Declaration of the publisher_ attribute
-  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr publisher_;
+  // Declaration of the publisher attribute for diffbot
+  rclcpp::Publisher<ros_phoenix::msg::MotorControl>::SharedPtr publisher_talon_right;
+
+  // Declaration of the publisher attribute for rrbot
+  rclcpp::Publisher<ros_phoenix::msg::MotorControl>::SharedPtr publisher_talon_left;
 };
 
 int main(int argc, char *argv[])
